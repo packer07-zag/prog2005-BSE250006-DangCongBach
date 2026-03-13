fbca05c0d90c32786ee7e28b7a7cc1330280f3cc
packer07-zag <bachdangcong2007@gmail.com>
2026-03-13 16:11:17 +0700

    Delete bai4.py

--- bai4.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-a=list(map(int,input().split()))
-for i in range(0,n):
-    for j in range(i+1,n):
-        if a[j]>a[i]:
-            s=a[j]
-            a[j]=a[i]
-            a[i]=s
-    print(a)
